1146cbb45424d48dd0eed26ee965ef7d1717d749
shumatsuura <shunsuke.mtr@gmail.com>
2020-01-30 14:41:22 +0900

    ソート機能拡充

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,23 @@
+$ (function() {
+    $('#index_sort').change(function() {
+
+      var html = "";
+
+      var this_value = $(this).val();
+
+      if (this_value == "due_ASC") {
+        html = "?sort=due_ASC"
+      } else if (this_value == "due_DESC") {
+        html = "?sort=due_DESC"
+      } else if (this_value == "created_at_ASC") {
+        html = "?sort=created_at_ASC"
+      } else if (this_value == "created_at_DESC") {
+        html = "?sort=created_at_DESC"
+      } else {
+        html = ""
+      };
+
+      window.location.href = "/" + html
+
+  });
+});
